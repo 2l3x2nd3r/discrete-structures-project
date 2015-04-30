5a5ff680b2bcfdbfce098e61cca5296022aedf15
Alexander Taborda <alextab93@gmail.com>
2015-04-30 17:21:55 -0500

    add attachment2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def attachment2_solution(array, k)
+    partitions = []
+    array.each_cons(k) { |item| partitions << item }
+    selectables = []
+
+    partitions.each do |partition|
+      i = partition.first
+      n = partition.last + 1
+      num1 = partition.reduce(:+)
+      dist = n - i
+      num2 = (n * (n + 1)) / (2) - (i * (i + 1)) / (2) - dist
+      if num1 != num2
+        selectables << partition
+      end
+    end
+    selectables
+  end
 end
--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,8 +1,18 @@
 class AttachmentsController < ApplicationController
+  def index
+  end
+
   def attachment1
   end
 
   def attachment2
+    if params[:array]
+      array = params[:array].split(',').map! { |item| item.to_i }
+      k = params[:k] != '' ? params[:k].to_i : 2
+      @selectables = attachment2_solution(array, k).paginate(page: params[:page], per_page: 5)
+    else
+      @selectables = []
+    end
   end
 
   def attachment3
