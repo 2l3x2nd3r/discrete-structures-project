d0d37af8382b27ac1a4bdb41c2f0eb836e3ac08a
Alexander Taborda <alextab93@gmail.com>
2015-05-07 00:20:34 -0500

    add limits for attachment 2

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -5,8 +5,14 @@ class AttachmentsController < ApplicationController
 
   def attachment2
     if params[:n]
+      n = params[:n].to_i
       k = params[:k] != '' ? params[:k].to_i : 3
-      @selectables = Attachment.attachment2_solution(params[:n].to_i, k).each_slice(10).to_a.paginate(page: params[:page], per_page: 10)
+      if n < 15 and k < 7
+        @selectables = Attachment.attachment2_solution(n, k).each_slice(10).to_a.paginate(page: params[:page], per_page: 10)
+      else
+        @selectables = []
+        @message = "N should be less than 15 and K less than 7"
+      end
     else
       @selectables = []
     end
