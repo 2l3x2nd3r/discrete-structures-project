339f800444562f73b14da567a0e2b7ee34ff6d14
Alexander Taborda <alextab93@gmail.com>
2015-05-08 18:57:36 -0500

    problem with pagination solve

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  
+
+  $last_selectables1 = []
+  $last_selectables2 = []
+  $last_selectables3 = []
 end
--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -4,7 +4,15 @@ class AttachmentsController < ApplicationController
     if params[:n]
       n = params[:n].to_i
       if n < 51
-        @selectables = Attachment.attachment1_solution(n).each_slice(5).to_a.paginate(page: params[:page], per_page: 5)
+        if params[:page]
+          @selectables = $last_selectables1
+          @selectables = @selectables.paginate(page: params[:page], per_page: 5)
+        else
+          @selectables = Attachment.attachment1_solution(n).each_slice(5).to_a
+          $last_selectables1 = @selectables
+          @selectables = @selectables.paginate(page: params[:page], per_page: 5)
+        end
+        
       else
         @selectables = []
         @message = "N should be less than 51"
@@ -19,7 +27,14 @@ class AttachmentsController < ApplicationController
       n = params[:n].to_i
       k = params[:k] != '' ? params[:k].to_i : 3
       if n < 15 and k < 7
-        @selectables = Attachment.attachment2_solution(n, k).each_slice(10).to_a.paginate(page: params[:page], per_page: 10)
+        if params[:page]
+          @selectables = $last_selectables2
+          @selectables = @selectables.paginate(page: params[:page], per_page: 10)
+        else
+          @selectables = Attachment.attachment2_solution(n, k).each_slice(10).to_a
+          $last_selectables2 = @selectables
+          @selectables = @selectables.paginate(page: params[:page], per_page: 10)
+        end
       else
         @selectables = []
         @message = "N should be less than 15 and K less than 7"
@@ -34,7 +49,14 @@ class AttachmentsController < ApplicationController
       array = params[:array].split(',').map! { |item| item.to_i }
       k = params[:k] != '' ? params[:k].to_i : 20
       if k < 51
-        @selectables = Attachment.attachment3_solution(array, k).each_slice(5).to_a.paginate(page: params[:page], per_page: 5)
+        if params[:page]
+          @selectables = $last_selectables3
+          @selectables = @selectables.paginate(page: params[:page], per_page: 5)
+        else
+          @selectables = Attachment.attachment3_solution(array, k).each_slice(5).to_a
+          $last_selectables3 = @selectables
+          @selectables = @selectables.paginate(page: params[:page], per_page: 5)
+        end
       else
         @selectables = []
       end
